74bcff0d1471981d97baa0ab2ac832003e69b0ce
Sergei Kraev <serega.kraev1@yandex.ru>
2025-11-27 00:27:32 +0300

    update js_string

--- js_simple/0.test/js.js
+++ js_simple/0.test/js.js
@@ -1,26 +1,16 @@
 "use strict";
-///////////////////////////////////////
-const book = {
-  title: '1984',
-};
 
-book.author = 'Джордж Оруэлл';
-book['year'] = 1949;
+function createGuest(name, age, bookingId) {
+    let obj = {
+      name: name,
+      age: age,
+      bookingId: bookingId
+    };
 
-const newKey = 'pages';
-book[newKey] = 328;
-///////////////////////////////////////
-
-function applyWeeklyRestock(bookCatalog) {
-
-    for (let key in bookCatalog) {
-        const book = bookCatalog[key];
-        bookCatalog[key] = book + 2;
-    }
-    return bookCatalog;
+    return obj;
 }
 
-console.log(applyWeeklyRestock({ "Герой нашего времени": 2, "Отцы и дети": 4, "Обломов": 1 }));
+console.log(createGuest('Иван Иванов', 25, 'BR123ABC'));
 
 
 

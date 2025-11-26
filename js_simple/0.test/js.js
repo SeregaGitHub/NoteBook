9f9213bf81065c8f78373cc7081ebc9d8e62ce7a
Sergei Kraev <serega.kraev1@yandex.ru>
2025-11-26 15:17:28 +0300

    js_switch

--- js_simple/0.test/js.js
+++ js_simple/0.test/js.js
@@ -1,17 +1,43 @@
 "use strict";
 
-const fishPerDay = [3, 8, 2, 5, 7, 1];
+function timeToBrewCoffee(name) {
+    switch (name) {
+        case 'Эспрессо': 
+            return 1;
+        case 'Американо': 
+            return 2;
+        case 'Капучино': 
+            return 2;
+        case 'Латте': 
+            return 3;
+        case 'Мокко': 
+            return 4;            
+        default:
+            return 2.5;
+    }
+};
 
-function fixFishCountLog(fishPerDay) {
 
-    for (let index = 0; index < fishPerDay.length; index++) {
-        if (index % 3 === 0) {
-            fishPerDay.splice(index, 1, fishPerDay[index] + 1);
+
+function pendingOrders(timeLeft, orders) {
+    
+    while (orders.length > 0) {
+        
+        if (timeLeft > 0) {
+            const nextOrder = orders.shift();
+            const orderTime = timeToBrewCoffee(nextOrder);
+            timeLeft = timeLeft - orderTime;
+        } else {
+            return orders;
         }
     }
 
-    return fishPerDay;
-}
+    return orders;
+};
 
-console.log(fixFishCountLog(fishPerDay));
+let timeLeft1 = 10;
+let orders1 = ["Капучино", "Латте", "Мокко"];
+
+console.log(pendingOrders(timeLeft1, orders1));
+
 
---
+++ js_simple/JS/switch.js
@@ -0,0 +1,13 @@
+"use strict";
+
+switch (day) {
+  case 'вторник':
+    console.log('Это вторник')
+    break
+  case 'среда':
+    console.log('Это среда')
+    break
+  case 'четверг':
+    console.log('Это четверг')
+    break
+}
